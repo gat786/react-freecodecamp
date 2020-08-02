22d12c2eac64bd2dba3798a99b2d77ac500b136d
Ganesh Tiwari <ganesht049@gmail.com>
2020-08-02 10:33:07 +0530

    to do items created

---
+++ src/todoList.js
@@ -0,0 +1,29 @@
+const todoList = {
+    tasks:[
+        {
+            "id" : 01,
+            "task" : "Take out trash",
+            "done" : false,
+        },
+        {
+            "id" : 02,
+            "task" : "Study Physics",
+            "done" : false,
+        },
+        {
+            "id" : 03,
+            "task" : "Learn An Algorithm",
+            "done" : false,
+        },
+        {
+            "id" : 04,
+            "task" : "Record the new session",
+            "done" : false,
+        },
+        {
+            "id" : 05,
+            "task" : "Talk to Dawuda",
+            "done" : false,
+        }
+    ]
+}
